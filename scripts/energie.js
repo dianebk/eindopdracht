f958d191f77293dcc4f3c73b08d91fd70c71a432
dianebk <104859451+dianebk@users.noreply.github.com>
2022-05-04 21:44:12 +0200

    Update energie.js
    url in script aangepast

--- scripts/energie.js
+++ scripts/energie.js
@@ -1,4 +1,4 @@
-fetch('https://raw.githubusercontent.com/dianebk/eindopdracht/cc887d5ca4d25208811c954abf0e6c1bb0011256/data/energie.csv?token=GHSAT0AAAAAABUHGU7CWENEIBFRJ75YOFJYYTS2V2A')
+fetch('https://raw.githubusercontent.com/dianebk/eindopdracht/cc887d5ca4d25208811c954abf0e6c1bb0011256/data/energie.csv?token=GHSAT0AAAAAABUHGU7DRDYK3U4YVSYKWJSWYTS23KQ')
 
     .then(function (response) {
         return response.text();
