befd65435fb07bc31a5c075c240a65f5b801dac3
dianebk <104859451+dianebk@users.noreply.github.com>
2022-05-07 14:02:05 +0200

    niet nodig map verwijderd
    niet nodig map verwijderd

--- niet nodig/glas.js
+++
@@ -1,9 +0,0 @@
-// const showModal = () => {
-//     document.getElementById('message').innerText = "Jaaaaa, gelukt!";
-//     document.getElementById('modal').classList.remove("hide");
-
-// }
-
-const hideModal = () => {
-    document.getElementById('modal').classList.add("hide");
-}
--- niet nodig/websitescript.js
+++
@@ -1,4 +0,0 @@
-// const heading = document.querySelector("h1");
-// let innerText = document.createTextNode(document.title);
-// heading.appendChild(innerText);
-// heading.classList.add('h1titel');
